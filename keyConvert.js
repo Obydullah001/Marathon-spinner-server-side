d90cf1030b83ecc48aaf05e56cbe4c494ebfa584
obydullah001 <hasibobydullah001@gmail.com>
2025-10-22 19:22:10 +0600

    Remove secret file and update project

--- keyConvert.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const key = fs.readFileSync('./firebase-adminsdk-servicekey-fbsvc-249b1b4609.json','utf-8')
-const base64 = Buffer.from(key).toString('base64')
-console.log(base64);
